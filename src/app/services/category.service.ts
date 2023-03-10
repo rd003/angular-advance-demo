1334a22c3090be9085668e3b4bd4e8fb060710ed
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-03-10 21:22:20 +0530

    category service on push

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Category } from 'src/models/category.model';
 
 @Injectable({
@@ -8,29 +9,61 @@ import { Category } from 'src/models/category.model';
 export class CategoryService {
 
   private apiUrl="http://localhost:3000/categories";
+  private categorySubject:BehaviorSubject<Category[]> = new BehaviorSubject<Category[]>([]);
 
-  addCategory(category:Category){
-    return this.http.post<Category>(this.apiUrl,category);
+  fetchCategories(){
+    return this.http.get<Category[]>(this.apiUrl).pipe(
+      tap(categories=>this.categorySubject.next(categories))
+    );
   }
 
-  updateCategory(id:string,category:Category){
-    return this.http.put<Category>(`${this.apiUrl}/${id}`,category);
+  getCategories():Observable<Category[]>{
+    if(this.categorySubject.getValue().length===0){
+      this.fetchCategories();
+    }
+    return this.categorySubject.asObservable();
   }
-
-  deleteCategory(id:string){
-    return this.http.delete<never>(`${this.apiUrl}/${id}`);
-  }
-
 
   getCategoryById(id:string){
     return this.http.get<Category>(`${this.apiUrl}/${id}`);
   }
 
-  getCategories(){
-    return this.http.get<Category[]>(this.apiUrl);
+
+  addCategory(category:Category):Observable<Category>{
+    return this.http.post<Category>(this.apiUrl,category).pipe(
+      tap(category=>{
+        const categories=this.categorySubject.getValue();
+        categories.push(category);
+        this.categorySubject.next(categories);
+      })
+    )
   }
 
-  
+  updateCategory(id:string,category:Category):Observable<Category>{
+    return this.http.put<Category>(`${this.apiUrl}/${id}`,category).pipe(
+      tap(category=>{
+        const categories= this.categorySubject.getValue();
+        const index= categories.findIndex(a=>a.id==category.id);
+        if(index!==-1){
+          categories[index]=category;
+          this.categorySubject.next(categories);
+        }
+      })
+    )
+  }
+
+  deleteCategory(id:string){
+    return this.http.delete<never>(`${this.apiUrl}/${id}`).pipe(
+      tap(_=>{
+         const categories= this.categorySubject.getValue();
+         const index= categories.findIndex(a=>a.id==id);
+         if(index!==-1){
+          categories.splice(index,1);
+          this.categorySubject.next(categories); 
+         }
+      })
+    )
+  }
 
   constructor(private http:HttpClient) { }
 }
