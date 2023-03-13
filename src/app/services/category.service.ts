433b84109bc7364db949ea2b1387607f62533411
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-03-13 13:51:00 +0530

    added pagination ,no logic implemented

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,11 +1,37 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, tap } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject, combineLatestWith, debounceTime, distinctUntilChanged, map, Observable, pipe, switchMap, tap } from 'rxjs';
 import { Category } from 'src/models/category.model';
 
-export interface CategoryResponse{
+export interface Pagination{
+  currentPage:number;
+  totalRecords:number;
+  totalPages:number;
+  selectedPageSize:number;
+  pageSizes:number[];
+}
+
+export interface CategoryState{
   categories:Category[],
-  count:number
+  totalRecords:number,
+  pagination:Pagination,
+  searchCriteria:string,
+  loading:boolean
+}
+
+let _state:CategoryState={
+  categories:[],
+  searchCriteria:'',
+  totalRecords:0,
+  loading:false,
+  pagination:{
+    currentPage:0,
+    selectedPageSize:5,
+    totalPages:0,
+    totalRecords:0,
+    pageSizes:[5,10,15,20]
+  }
 }
 
 @Injectable({
@@ -14,43 +40,173 @@ export interface CategoryResponse{
 export class CategoryService {
 
   private apiUrl="http://localhost:3000/categories";
+  private store=new BehaviorSubject<CategoryState>(_state);
+  private state$=this.store.asObservable();
 
-  getCategories(page=1,limit=10,searchTerm=''):Observable<CategoryResponse>{
+  categories$=this.state$.pipe(
+    map(state=>state.categories),
+    distinctUntilChanged()
+    );
+
+  pagination$=this.state$.pipe(
+      map(state=>state.pagination),
+      distinctUntilChanged()
+      );
+  searchCriteria$=this.store.pipe(
+        map(state=>state.searchCriteria),
+        distinctUntilChanged()
+        );
+  
+  loading$= this.state$.pipe(
+    map(state=>state.loading)
+  )
+
+  totalRecords$=this.state$.pipe(
+    map(state=>state.totalRecords)
+  )
+
+  /**
+   * Viewmodel that resolves once all the data is ready (or updated)...
+   */
+
+  vm$:Observable<CategoryState>=this.pagination$.pipe(
+    combineLatestWith(
+      this.searchCriteria$,
+      this.categories$,
+      this.loading$,
+      this.totalRecords$
+    ),
+    map(([pagination,searchCriteria,categories,loading,totalRecords])=>
+    ({pagination,searchCriteria,categories,loading,totalRecords}) //array destructuring
+  ));
+
+  
+  /**
+   * rest api calls
+   */
+
+  private getCategories(searchCriteria:string,pagination:Pagination):Observable<Category[]>{
     // const url = `${this.apiUrl}?_page=${page}&_limit=${limit}&name=${searchTerm}`;
-    let params= new HttpParams()
-                  .set('_page',page.toString())
-                  .set('_limit',limit.toString())
-    if(searchTerm){
-      params=params.set('q',searchTerm)
-    }
-    const url= `${this.apiUrl}?${params.toString()}`;
-   return this.http.get(url,{observe:'response'})
+    
+    const url= this.buildCategoryUrl(searchCriteria,pagination);
+    return this.http.get(url,{observe:'response'})
            .pipe(
             map(response=> {
-              const count= parseInt(response.headers.get('X-Total-Count')||"0",10);
-              const categories= response.body as Category[]
-              return {count,categories} as CategoryResponse;
-            })
+              const totalRecords= parseInt(response.headers.get('X-Total-Count')||"0",10);
+              const categories= response.body as Category[];
+              this.updateState({..._state,totalRecords})
+              return categories;
+            }),
+            map( filterWithCriteria(searchCriteria))
            )
   }
-
-
-  getCategoryById(id:string){
-    return this.http.get<Category>(`${this.apiUrl}/${id}`);
-  }
-
-  addCategory(category:Category):Observable<Category>{
-    return this.http.post<Category>(this.apiUrl,category);
-  }
-
-  updateCategory(id:string,category:Category):Observable<Category>{
-    return this.http.put<Category>(`${this.apiUrl}/${id}`,category);
-  }
-
-  deleteCategory(id:string){
-    return this.http.delete<never>(`${this.apiUrl}/${id}`);
-  }
+  
+  /**
+   * Watch 2 streams to trigger user loads and state updates
+   */
+ 
 
   constructor(private http:HttpClient) { 
+    this.searchCriteria$.pipe(
+      combineLatestWith(this.pagination$),
+      switchMap(([searchCriteria,pagingation])=>{
+        return this.getCategories(searchCriteria,pagingation)
+      }))
+      .subscribe(categories=>{
+      this.updateState({..._state,categories,loading:false})
+    })
   }
+
+  /**
+   *public methods
+   */
+
+  // Allows quick snapshot access to data for ngOnInit() purposes
+  getStateSnapshot(): CategoryState {
+    return { ..._state, pagination: { ..._state.pagination } };
+  }
+
+ 
+  buildSearchTermControl():FormControl{
+     const searchTerm=new FormControl();
+     const searchTerm$=searchTerm.valueChanges
+                               pipe(
+                                  debounceTime(300),
+                                  distinctUntilChanged()
+                                  );
+     searchTerm$.subscribe(value=>{
+      this.updateSearchCriteria(value);
+     });
+
+     return searchTerm;
+  }
+
+  updateSearchCriteria(searchCriteria:string){
+    this.updateState({..._state,searchCriteria,loading:true})
+
+  }
+
+  updatePagination(selectedSize: number, currentPage: number = 0) {
+    const pagination = { ..._state.pagination, currentPage, selectedSize };
+    this.updateState({ ..._state, pagination, loading: true });
+  }
+
+
+  /**
+   * private methods
+   */
+
+  private buildCategoryUrl(searchCriteria:string='',pagination:Pagination) {
+    let params= new HttpParams()
+      .set('_page',pagination.currentPage.toString())
+      .set('_limit',pagination.selectedPageSize.toString());
+    // add a new parameter to url if searchCriterial is not empty or null
+    /*
+    if(searchCriteria){
+      params=params.set('q',searchCriteria)
+    }
+    */
+    const url= `${this.apiUrl}?${params.toString()}`;
+    return url;
+  }
+
+  private updateState(state:CategoryState){
+    _state=state;
+     this.store.next(_state);
+  }
+
+  /** This code will not work, i wonder why 🤔 */
+  // private updateState(state:CategoryState){
+  //    this.store.next(state);
+  // }
+
+
 }
+
+// ******************************************
+// Filter Utilities
+// ******************************************
+
+
+function filterWithCriteria(searchCriteria: string) {
+
+  return (categories: Category[]) => {
+    if (!searchCriteria) {
+      return categories;
+    }
+
+    const filteredCategories = categories.filter(category => {
+      return category.name.toLowerCase().startsWith(searchCriteria.toLowerCase());
+    });
+
+    return filteredCategories;
+  };
+}
+
+
+
+
+
+
+    
+   
